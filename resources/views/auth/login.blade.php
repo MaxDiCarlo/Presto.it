cf2074e4c276aa0bd9486d2decbeed491b71b480
Pasquale Russo <pasquale.russo@azinfocollection.it>
2024-07-11 10:40:00 +0200

    Prime modifiche lato frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,16 +1,20 @@
 <x-layout>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
+    <div class="min-vh-25 mt-5">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
     </div>
-    @endif
+    
+</div>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 pt-5">
                 <form action="{{route('login')}}" method="POST">
                     @csrf
                     <div class="mb-3">
