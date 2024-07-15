9045a6c156bb2fef29d5c4cedcb3d7f0dd79aa96
Lorenzo Dasara <lorenzodasara123@gmail.com>
2024-07-15 16:35:14 +0200

    modifica logo navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,7 +4,7 @@
     <div class="col-12 col-md-6">
       <nav class="navbar navbar-expand-lg fixed-top">
          <div class="container-fluid">
-           <a class="navbar-brand px-3" href="{{route('homepage')}}">Presto.it</a>
+           <a class="navbar-brand px-3" href="{{route('homepage')}}"><img src="../images/logofinale.png" alt=""></a>
            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
              <span class="navbar-toggler-icon"></span>
            </button>
