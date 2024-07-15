5f348233ff6e45e89c9751342da45495746e2c95
Massimiliano Di Carlo <massimilianodica@gmail.com>
2024-07-15 22:07:56 +0200

    sistemata homepage, cambiata palette colori, aggiunto logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,87 +1,65 @@
-{{-- navbar da modificare --}}
-
 <div class="container">
-    <div class="row">
-      <!-- Colonna per il logo -->
-      <div class="col-12 col-md-6">
-        <a href="{{ route('homepage') }}">
-          <img src="/images/logo.png" width="70" height="100" alt="Logo del sito">
-        </a>
-      </div>
-    <div class="col-12 col-md-6">
-      <nav class="navbar navbar-expand-lg fixed-top">
-        <div class="navbar-logo">
-          <a href="{{route('homepage')}}">
-              <img src="/images/logo.png" width="70" height="100" alt="Logo del sito">
-            </div>
-         <div class="container-fluid">
+    <div class="row align-items-center">
+        <!-- Colonna per il logo -->
+        <div class="col-12 col-md-6 fixed-top">
+            <a href="{{ route('homepage') }}">
+                <img src="/images/logo.png" width="100" height="100" alt="Logo del sito">
             </a>
-           <a class="navbar-brand px-3" href="{{route('homepage')}}"><img src="" alt=""></a>
-           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-             <span class="navbar-toggler-icon"></span>
-           </button>
-           <div class="collapse navbar-collapse" id="navbarSupportedContent">
-             <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
-               <li class="nav-item">
-                 <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
-               </li>
-               <li class="nav-item">
-                 <a class="nav-link" href="{{route('advertise.index')}}">Latest news</a>
-               </li>
-               @guest
-               <li class="nav-item">
-                 <a class="nav-link " href="{{route('login')}}">Login</a>
-               </li>
-               <li class="nav-item">
-                 <a class="nav-link" href="{{route('register')}}">Register</a>
-               </li>
-               @endguest
-               @auth
-                 <li class="nav-item">
-                   <a class="nav-link" href="{{route('advertise.create')}}">Inserisci annuncio</a>
-                 </li>
-                 @if (Auth::user()->reviewer)           
-                   <li class="nav-item">
-                     <a class="nav-link" href="{{route('reviewer.area')}}">Area revisori</a>
-                   </li>
-                 @else
-                   <li class="nav-item">
-                     <a class="nav-link" href="{{route('reviewer.richiesta')}}">Lavora con noi</a>
-                   </li>
-                 @endif
-                 <li class="nav-item dropdown">
-                   <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     Benvenuto {{Auth::user()->name}}
-                   </a>
-                   <ul class="dropdown-menu mt-3">
-                     <li">
-                       <form class="d-flex justify-content-center" action="{{route('logout')}}" method="POST">
-                         @csrf
-                         <button type="submit" class="nav-link text-dark">Logout</button>
-                       </form>
-                     </li>
-                   </ul>
-               @endauth
-             </ul>
-             <form class="d-flex" role="search">
-               <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
-               <button class="btn btn-outline-light me-3" type="submit">Search</button>
-             </form>
-           </div>
-         </div>
-       </nav>
+        </div>
+        <div class="col-12 col-md-6">
+            <nav class="navbar navbar-expand-lg fixed-top">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="{{route('homepage')}}">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('advertise.index')}}">Latest news</a>
+                            </li>
+                            @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('login')}}">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('register')}}">Register</a>
+                            </li>
+                            @endguest
+                            @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('advertise.create')}}">Inserisci annuncio</a>
+                            </li>
+                            @if (Auth::user()->reviewer)
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('reviewer.area')}}">Area revisori</a>
+                            </li>
+                            @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('reviewer.richiesta')}}">Lavora con noi</a>
+                            </li>
+                            @endif
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Benvenuto {{Auth::user()->name}}
+                                </a>
+                                <ul class="dropdown-menu mt-3">
+                                    <li>
+                                        <form class="d-flex justify-content-center" action="{{route('logout')}}" method="POST">
+                                            @csrf
+                                            <button type="submit" class="nav-link text-dark">Logout</button>
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                            @endauth
+                        </ul>
+                        <form class="d-flex" role="search">
+                            <input class="form-control me-1" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-light me-3" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </div>
 </div>
-
-{{-- <div class="container text-center">
-  <div class="row">
-    <div class="col-12 col-md-6">
-      1 of 2
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-12 col-md-6">
-      1 of 3
-    </div>
-  </div>
-</div> --}}
