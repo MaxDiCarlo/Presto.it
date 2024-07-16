ebc26ddc6f7606a4ac7d847cd056ceda5d0cb5a8
Tiziano Di Felice <tizianofelice@gmail.com>
2024-07-16 22:45:07 +0200

    tolta condizione inutile

--- resources/views/advertise/index.blade.php
+++ resources/views/advertise/index.blade.php
@@ -1,35 +1,18 @@
 <x-layout>
     <x-masthead2></x-masthead2>
         <div class="container-fluid">
-            @if (isset($condizione))
-                    @if (count($advertises) > 0 && $condizione)
-                    <div class="row justify-content-around">
-                    @foreach($advertises as $advertise)
-                    <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
-                        <x-card :advertise="$advertise"/>
-                    </div>
-                    @endforeach
-                @else
-                        <div class="row justify-content-center my-5">
-                            <h1>Nessun annuncio da mostrare</h1>
-                        </div>
-                @endif
-
+            @if (count($advertises) > 0)
+                <div class="row justify-content-around my-5">
+                @foreach($advertises as $advertise)
+                <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
+                    <x-card :advertise="$advertise"/>
+                </div>         
+                @endforeach
             @else
-                    @if (count($advertises) > 0)
-                    <div class="row justify-content-around my-5">
-                    @foreach($advertises as $advertise)
-                    <div class="col-12 col-md-4 mb-5 d-flex justify-content-center">
-                        <x-card :advertise="$advertise"/>
-                    </div>         
-                    @endforeach
-                @else
-                        <div class="row justify-content-center my-5">
-                            <h1>Nessun annuncio da mostrare</h1>
-                        </div>
-                @endif
-                    </div>
-                @endif
+                <div class="row justify-content-center my-5">
+                    <h1>Nessun annuncio da mostrare</h1>
+                </div>
+            @endif
+            </div>
         </div>
-
 </x-layout>
