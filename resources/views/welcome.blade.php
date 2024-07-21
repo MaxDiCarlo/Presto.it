83a0495f84398e763e156cdb09f54f1f70b3005a
Lorenzo Dasara <lorenzodasara123@gmail.com>
2024-07-22 00:51:04 +0200

    inserimento sezione in home delle card delle categorie

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,66 @@
     <div class="container-fluid my-5 py-5">
         @if (count($advertises) > 0)
             <div class="row mb-5">
+
+                <div class="container-fluid container-area">
+                    <div class="row justify-content-around mt-5 m-0">
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Sport</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Cinema</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                                
+                            <h2>Intrattenimento</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid container-area">
+                    <div class="row justify-content-around mt-5 m-0">
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Videogiochi</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Cucina</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                                
+                            <h2>Programmazione</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container-fluid container-area">
+                    <div class="row justify-content-around m-0">
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Viaggi</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                            
+                            <h2>Manga</h2>
+                        </div>
+                        <div class="col-12 col-md-3 text-center option p-0">
+                                
+                            <h2>Musica</h2>
+                        </div>
+                    </div>
+                </div>
+            
+                <script>
+                    document.querySelectorAll('.option').forEach(element => {
+                        element.addEventListener('click', () => {
+                            window.location.href = element.getAttribute('data-url');
+                        })
+                    })
+                </script>
+
                 <div class="col-12 d-flex justify-content-center">
                     <h2 class="presentation-h2">{{__('ui.latest articles')}}</h2>
                 </div>
@@ -26,4 +86,7 @@
         @endif
             </div>
     </div>
+
+    {{-- <div class="bg"></div> --}}
+
 </x-layout>
