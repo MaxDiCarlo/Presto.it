b25952493fd7a0fec22f8e5d573ed2c623e7f00e
Tiziano Di Felice <tizianofelice@gmail.com>
2024-07-22 15:29:15 +0200

    user story 8 fatta

--- app/Livewire/FormCreazione.php
+++ app/Livewire/FormCreazione.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Jobs\GoogleVisionLabelImage;
 use App\Jobs\GoogleVisionSafeSearch;
+use App\Jobs\RemoveFaces;
 use App\Jobs\ResizeImage;
 use Livewire\Component;
 use App\Models\Category;
@@ -81,9 +82,15 @@ class FormCreazione extends Component
             foreach($this->images as $image){
                 $newFileName = "advertises/{$advertise->id}";
                 $newImage = $advertise->images()->create(['path' => $image->store($newFileName, 'public')]);
-                dispatch(new ResizeImage($newImage->path,1800, 1800));
-                dispatch(new GoogleVisionSafeSearch($newImage->id));
-                dispatch(new GoogleVisionLabelImage($newImage->id));
+                // dispatch(new ResizeImage($newImage->path,1800, 1800));
+                // dispatch(new GoogleVisionSafeSearch($newImage->id));
+                // dispatch(new GoogleVisionLabelImage($newImage->id));
+                // dispatch(new RemoveFaces($newImage->id));
+                RemoveFaces::withChain([
+                    new ResizeImage($newImage->path, 1800, 1800),
+                    new GoogleVisionSafeSearch($newImage->id),
+                    new GoogleVisionLabelImage($newImage->id)
+                ])->dispatch($newImage->id);
             }
             File::deleteDirectory(storage_path('/app/livewire-tmp'));
         }
